bf69f1e2119a3e006b0b2a30aa6a9fa2f9c9b052
Luc <luc.helmlinger@epitech.eu>
2026-01-28 16:18:00 +0900

    feat: level selection, new prefab for sceneManagement

--- Assets/Scripts/Systems/SceneManagerSystem.cs
+++ Assets/Scripts/Systems/SceneManagerSystem.cs
@@ -1,46 +1,41 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LoadingScene : MonoBehaviour
 {
-    public GameObject loadingScreen;
-    public Image loadingBar;
+    public static LoadingScene Instance { get; private set; }
 
-    //
-    // Summary:
-    //     Change scene WITHOUT a loading screen
-    public void ChangeScene(string scene)
+    public GameObject loadingScreen;
+    public UnityEngine.UI.Image loadingBar;
+
+    private void Awake()
     {
-        SceneManager.LoadSceneAsync(scene);
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        // DontDestroyOnLoad(gameObject);
     }
 
-    //
-    // Summary:
-    //     Change scene WITH a loading screen
     public void LoadScene(string scene)
     {
-        // TODO Create the loading screen object here & create a public method to instantiate the loading screen prefab.
         StartCoroutine(LoadSceneAsync(scene));
     }
 
-    IEnumerator LoadSceneAsync(string scene)
+    private IEnumerator LoadSceneAsync(string scene)
     {
         loadingScreen.SetActive(true);
+
         AsyncOperation op = SceneManager.LoadSceneAsync(scene);
+        op.allowSceneActivation = true;
 
         while (!op.isDone) {
             float progressValue = Mathf.Clamp01(op.progress / 0.9f);
-
             loadingBar.fillAmount = progressValue;
-
             yield return null;
         }
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }
